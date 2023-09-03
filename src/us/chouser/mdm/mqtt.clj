99f4b6231a81aa9627111d0860277e54492a5ca2
Chouser <chouser@n01se.net>
2023-09-03 19:33:23 -0400

    Add mqtt session metrics

--- src/us/chouser/mdm/mqtt.clj
+++ src/us/chouser/mdm/mqtt.clj
@@ -1,4 +1,5 @@
 (ns us.chouser.mdm.mqtt
+  (:require [us.chouser.mdm.sqlite-stats :as ss])
   (:import (org.eclipse.paho.mqttv5.client
             IMqttMessageListener MqttCallback MqttClient MqttConnectionOptions)
            (org.eclipse.paho.mqttv5.client.persist MemoryPersistence)
@@ -6,11 +7,15 @@
 
 (set! *warn-on-reflection* true)
 
+(ss/register-metrics
+ {::session    {:tag-types {:event :string}}})
+
 (defn array-of [sym]
   (.getName (class (into-array (resolve sym) []))))
 
 (defn subscribe [^MqttClient client {:keys [topic qos msg-fn]}]
   (println "mqtt subscribing" topic)
+  (ss/record ::session {:event :attempt-subscribe} 1)
   (let [sub (MqttSubscription. topic (or qos 1))]
     {:subscription sub
      :token (.subscribe client
@@ -23,38 +28,44 @@
 
 (defn connect [a]
   (let [{:keys [address client-id subs]} (:config @a)
-        client (MqttClient. address client-id (MemoryPersistence.))]
-    (swap! a assoc :client
-           (doto client
-             (.setCallback
-              (reify MqttCallback
-                (disconnected [_ resp]
-                  (println "mqtt disconnected:" resp)
-                  (loop []
-                    (Thread/sleep 5000)
-                    (when (and (not (:stop? @a))
-                               (try (connect a)
-                                    false
-                                    (catch Exception ex
-                                      (println "mqtt connect failed:" (.getMessage ex))
-                                      true)))
-                      (recur))))
-                (mqttErrorOccurred [_ ex]
-                  (println "mqtt error occurred:" ex))
-                (messageArrived [_ topic message]
-                  :ignore)
-                (deliveryComplete [_ token]
-                  (println "mqtt delivery complete:" token))
-                (connectComplete [_ reconnect uri]
-                  (println "mqtt connect complete:" reconnect uri))
-                (authPacketArrived [_ reasonCode properties]
-                  (println "mqtt auth packet arrived:" reasonCode properties))))
-             (.connect (doto (MqttConnectionOptions.)
-                         (.setConnectionTimeout 5)
-                         ;; We're not trying to receive every message, but instead
-                         ;; monitor that fresh messages are being received. Use
-                         ;; CleanStart to disable message persistence:
-                         (.setCleanStart true)))))
+        client (MqttClient. address client-id (MemoryPersistence.))
+        callback
+        , (reify MqttCallback
+            (disconnected [_ resp]
+              (println "mqtt disconnected:" resp)
+              (ss/record ::session {:event :disconnect} 1)
+              (loop []
+                (Thread/sleep 5000)
+                (when (and (not (:stop? @a))
+                           (try (connect a)
+                                false
+                                (catch Exception ex
+                                  (println "mqtt connect failed:" (.getMessage ex))
+                                  true)))
+                  (recur))))
+            (mqttErrorOccurred [_ ex]
+              (ss/record ::session {:event :error} 1)
+              (println "mqtt error occurred:" ex))
+            (messageArrived [_ topic message]
+              (ss/record ::session {:event :message} 1))
+            (deliveryComplete [_ token]
+              (ss/record ::session {:event :delivered} 1)
+              (println "mqtt delivery complete:" token))
+            (connectComplete [_ reconnect uri]
+              (ss/record ::session {:event :connected} 1)
+              (println "mqtt connect complete:" reconnect uri))
+            (authPacketArrived [_ reasonCode properties]
+              (ss/record ::session {:event :auth-packet} 1)
+              (println "mqtt auth packet arrived:" reasonCode properties)))]
+    (swap! a assoc :client client)
+    (.setCallback client callback)
+    (ss/record ::session {:event :attempt-connect} 1)
+    (.connect client (doto (MqttConnectionOptions.)
+                       (.setConnectionTimeout 5)
+                       ;; We're not trying to receive every message, but instead
+                       ;; monitor that fresh messages are being received. Use
+                       ;; CleanStart to disable message persistence:
+                       (.setCleanStart true)))
     (->> subs (run! #(subscribe client %)))
     a))
 
